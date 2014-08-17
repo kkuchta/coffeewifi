21bda6a5615035866528d156024fa2c465b82cdd
Kevin Kuchta <kevin@kevinkuchta.com>
2014-08-16 18:31:00 -0700

    Integrated speed test

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   resources :businesses, only: [:show]
 
+  # This is used by the mini speedtest plugin.
+  post '/mini/speedtest/upload.rb' => 'speedtest#upload'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
